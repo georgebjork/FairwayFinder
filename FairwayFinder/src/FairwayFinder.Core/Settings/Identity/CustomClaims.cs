3dc8e7a5466468092e7a4bdcac9e7976518d0239
George <georgebjork@outlook.com>
2024-04-21 19:36:46 -0700

    add user claims for first name and last name so they are now in the http context

---
+++ FairwayFinder/src/FairwayFinder.Core/Settings/Identity/CustomClaims.cs
@@ -0,0 +1,7 @@
+namespace FairwayFinder.Core.Settings.Authorization;
+
+public static class CustomClaims
+{
+    public static string FirstName => "FirstName";
+    public static string LastName =>  "LastName";
+}
