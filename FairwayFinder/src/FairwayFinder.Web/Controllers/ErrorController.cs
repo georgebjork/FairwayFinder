b3d4a8d6a46439941e5c9630cf31dea5240ee32d
George Bjork <georgebjork@outlook.com>
2024-04-29 20:47:04 -0700

    add basic styling and htmx redirects just in case on error pages

--- FairwayFinder/src/FairwayFinder.Web/Controllers/ErrorController.cs
+++ FairwayFinder/src/FairwayFinder.Web/Controllers/ErrorController.cs
@@ -8,18 +8,21 @@ public class ErrorController : BaseController
     [Route("/401")]
     public IActionResult NotAuthorized()
     {
+        Response.Headers["HX-Redirect"] = Url.Action(nameof(NotAuthorized));
         return View();
     }
 
     [Route("/404")]
     public IActionResult NotFoundResponse()
     {
+        Response.Headers["HX-Redirect"] = Url.Action(nameof(NotFoundResponse));
         return View();
     }
     
     [Route("/500")]
     public IActionResult ServerError()
     {
+        Response.Headers["HX-Redirect"] = Url.Action(nameof(ServerError));
         return View();
     }
 }
