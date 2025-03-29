a9fedc5b8d8e4efb6f91f08a619d2feddbce4e60
George Bjork <georgebjork@outlook.com>
2025-03-29 09:38:54 -0700

    change to pgweb

--- FairwayFinder/src/FairwayFinder.AppHost/Program.cs
+++ FairwayFinder/src/FairwayFinder.AppHost/Program.cs
@@ -1,14 +1,15 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-// Generate random username and password for the PostgreSQL database
+// postgres username and password
 var username = builder.AddParameter("postgres-username", "postgres");
 var password = builder.AddParameter("postgres-password", "password");
 
-var postgres = builder.AddPostgres("postgres", username, password, port: 5432).WithPgAdmin(containerName: "pgadmin")
-    .WithVolume("data-fairwayfinder", "/var/lib/postgresql/data"); // ✅ correct default mount path
+// Add postgres with pg web
+var postgres = builder.AddPostgres("postgres", username, password, port: 5432).WithPgWeb(containerName: "pgweb")
+    .WithVolume("data-fairwayfinder", "/var/lib/postgresql/data");
 
+// Add database
 var db_fairwayfinder = postgres.AddDatabase("fairwayfinder", "db-fairwayfinder");
-
 
 // Add web project
 builder.AddProject<Projects.FairwayFinder_Web>("webapp")
