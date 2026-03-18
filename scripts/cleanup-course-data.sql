62ca34f6e36edb58cb0196a0c00c3023258ff988
georgebjork <georgebjork@outlook.com>
2026-03-18 14:04:31 -0700

    Update cleanup-course-data.sql

--- scripts/cleanup-course-data.sql
+++ scripts/cleanup-course-data.sql
@@ -30,3 +30,21 @@ delete_maps AS (
 )
 DELETE FROM course
 WHERE course_id IN (SELECT course_id FROM courses_to_delete);
+
+
+-- Test results
+SELECT c.course_id, c.course_name, c.city, c.state, c.country,
+       (SELECT COUNT(*) FROM teebox t WHERE t.course_id = c.course_id AND NOT t.is_deleted) AS teebox_count
+FROM course c
+WHERE NOT c.is_deleted
+  AND (
+    c.state IS NULL
+        OR c.country IS NULL
+        OR c.city IS NULL
+        OR c.course_name !~ '[a-zA-Z]'
+        OR NOT EXISTS (
+        SELECT 1 FROM teebox t
+        WHERE t.course_id = c.course_id AND NOT t.is_deleted
+    )
+    )
+ORDER BY c.course_name;
